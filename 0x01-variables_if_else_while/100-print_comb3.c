7bf552278e976fec34d58f8ca3dfc8cd61c4df85
Glacielar <glacielarwayua@gmail.com>
2023-02-18 13:53:50 -0800

    100 print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print all possible combination of two single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	d = 0;
+	while
+		(d < 10) {
+
+			c = 0;
+			while
+				(c < 10) {
+					if
+						(d != c && d < c) {
+							putchar('0' + d);
+							putchar('0' + c);
+
+							if
+								(c + d != 17) {
+									putchar(',');
+									putchar(' ');
+								}
+						}
+					c++;
+				}
+			c++;
+
+		}
+	putchar('\n');
+	return (0);
+}
