e24a69e8e30b560d46f07a55def11d006d8b48c5
Glacielar <glacielarwayua@gmail.com>
2023-02-18 04:45:15 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print alphabet in lowercase then upercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'c';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+while
+	(c <= 'Z') {
+putchar(c);
+c++;
+	}
+	putchar('\n');
+return (0);
+}
