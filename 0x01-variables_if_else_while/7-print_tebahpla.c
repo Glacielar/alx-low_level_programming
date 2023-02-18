92ad711ba9b21ae82c6adbbc421880b689aa87c8
Glacielar <glacielarwayua@gmail.com>
2023-02-18 07:16:14 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print letters in lowercase in reverse
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+c = 'z';
+while
+	(c <= 'a') {
+		putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
