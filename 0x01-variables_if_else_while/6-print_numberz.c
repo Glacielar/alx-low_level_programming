f64da9a4640cbcb7fbf7f68928229365be965d30
Glacielar <glacielarwayua@gmail.com>
2023-02-18 06:50:23 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print all single digits of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(48 + c);
+c++;
+}
+putchar('\n');
+return (0);
+}
