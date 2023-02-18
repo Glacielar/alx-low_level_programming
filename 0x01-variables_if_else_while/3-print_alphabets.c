abb3d52278902766c99a83b105c9ebfcc0565396
Glacielar <glacielarwayua@gmail.com>
2023-02-18 05:26:30 -0800

    alpHABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,11 +14,14 @@ int main(void)
 			putchar(c);
 			c++;
 		}
-while
-	(c <= 'Z') {
-putchar(c);
-c++;
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
