0a308d2c5aa621b4797b23edea61c12b8a855928
Glacielar <glacielarwayua@gmail.com>
2023-02-24 12:34:00 -0800

    update betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_square: prints a square followed by a new line
+ * print_square -  prints a square followed by a new line
+ * Description: If size is 0 or less, the function should print only a new line
  * @size: size of the square
  */
 
