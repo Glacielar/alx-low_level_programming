42e58136f26bbc088fdf69b349109e4dbc69a5e1
Glacielar <glacielarwayua@gmail.com>
2023-04-08 13:52:20 -0700

    tasks on bitwise manipulation update

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @index: index of the bit
+ * @n: number to search
+ *
+ * Return: value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 20)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+	return (bit_val);
+}
